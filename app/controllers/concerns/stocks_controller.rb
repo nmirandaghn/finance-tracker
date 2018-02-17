e5be9058ce3da8a0629a254786c96ae9ce0024c6
Nelson Miranda <nmirandaghn@gmail.com>
2018-02-17 13:19:14 -0600

    Added validation when stock does not exist

--- app/controllers/concerns/stocks_controller.rb
+++
@@ -1,7 +0,0 @@
-class StocksController < ApplicationController
-  def search
-    return if params[:stock].nil?
-    @stock = Stock.new_from_lookup(params[:stock])
-    render 'user/my_portfolio'
-  end
-end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,11 +1,17 @@
 class Stock < ApplicationRecord
   def self.new_from_lookup(ticker_symbol)
-    looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
-    price = strip_commas(looked_up_stock.l)
-    new(name: looked_up_stock.name, ticker: looked_up_stock.symbol, last_price: price)
+    begin
+      looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+      price = strip_commas(looked_up_stock.l)
+      new(name: looked_up_stock.name,
+          ticker: looked_up_stock.symbol,
+          last_price: price)
+    rescue StandardError => e
+      return nil
+    end
   end
 
   def self.strip_commas(number)
-    number.gsub(',', '')
+    number.delete(',')
   end
 end
