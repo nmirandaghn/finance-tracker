4a19c771180a3603bab8e2375320466966a97dee
Nelson Miranda <nmirandaghn@gmail.com>
2018-02-18 22:04:47 -0600

    Correction on form my_portfolio

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -2,11 +2,12 @@ class UserStocksController < ApplicationController
   def create
     stock = Stock.find_by_ticker(params[:stock_ticker])
     if stock.blank?
-      stock = Stock.new(params[:stock_ticker])
+      stock = Stock.new_from_lookup(params[:stock_ticker])
       stock.save
     end
     @user_stock = UserStock.create(user: current_user, stock: stock)
-    flash[:success] = "Stock #{@user_stock.stock.name} was succesfully "
+    flash[:success] = "Stock #{@user_stock.stock.name} was succesfully added to portfolio"
+    redirect_to my_portfolio_path
   end
 
   def self.find_by_ticker(ticker_symbol)
