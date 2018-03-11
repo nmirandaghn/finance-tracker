980a591a55a939a79a617f9fed8aa582993d9f2a
Nelson Miranda <nmirandaghn@gmail.com>
2018-03-11 16:10:54 -0600

    Create friends layout

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
+  get 'my_friends', to: 'users#my_friends'
+  resources :users, only: [:show]
+  resources :friendships
   resources :user_stocks, only: [:create, :destroy]
 end
